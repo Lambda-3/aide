4c304a5cdd7d317c9d91dc00632bf1df1e86eb97
L0n3Sn1p3r <vigi@vigis-web.com>
2017-11-16 10:38:25 +0100

    new message api

---
+++ aide_core/src/aide_core/apis/weather.py
@@ -0,0 +1,43 @@
+import subprocess
+
+import requests
+import json
+
+from aide_core import credentials
+
+API_KEY = credentials.OPEN_WEATHER_MAP_API_KEY
+ENDPOINT = "http://api.openweathermap.org/data/2.5/weather?&units=metric&appid={}".format(API_KEY)
+
+
+def get_weather(latitude, longitude):
+    """
+
+    :param latitude:
+    :type: float
+    :param longitude:
+    :type: float
+    :return:
+    """
+    headers = {
+        'content-type': "application/json"
+    }
+    link = ENDPOINT+ "&lat={}&lon={}".format(latitude, longitude)
+    response = requests.request("GET", link, headers=headers)
+
+    response.raise_for_status()
+
+    response = response.json()
+    print type(response['weather'])
+    return {
+        "weather_type": response['weather'][0]['description'],
+        "temperature": response['main']['temp'],
+        "city": response['name'],
+        "country": response['sys']['country']
+    }
+
+
+
+
+def get_weather_human_readable(lat, lng):
+    weather= get_weather(lat, lng)
+    return """The weather at {city}, {country} is {weather_type}, it has {temperature} degree(s).""".format(**weather)
